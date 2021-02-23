7c041b4315e426b836d29227c8fb854911c6940d
rika.mori <rika.mori@rei-frontier.jp>
2021-02-23 14:43:50 +0900

    AndroidQ対応

--- app/src/main/java/com/studio/jozu/mocklocation/activity/MainViewModel.kt
+++ app/src/main/java/com/studio/jozu/mocklocation/activity/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.studio.jozu.mocklocation.activity
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -16,7 +17,11 @@ import com.google.android.gms.maps.model.MarkerOptions
  */
 class MainViewModel(private val activity: MainActivity) {
     companion object {
-        val LOCATION_PERMISSIONS = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+        val LOCATION_PERMISSIONS = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+        } else {
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+        }
         val TOKYO_STATION_LAT_LNG = LatLng(35.6809591, 139.7673068)
         val HOME_LAT_LNG = LatLng(36.325704, 137.831932)
     }
--- app/src/main/java/com/studio/jozu/mocklocation/broadcast/LocationReceiver.kt
+++ app/src/main/java/com/studio/jozu/mocklocation/broadcast/LocationReceiver.kt
@@ -3,6 +3,7 @@ package com.studio.jozu.mocklocation.broadcast
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import org.greenrobot.eventbus.EventBus
@@ -32,7 +33,11 @@ class LocationReceiver : BroadcastReceiver() {
         //val jsonString = "{\"speed\":4.0,\"repeat\":-1,\"reverse\":true,\"delay\":5000,\"coordinates\":[{\"latitude\":36.325928,\"longitude\":137.832105},{\"latitude\":36.326135, \"longitude\":137.832173},{\"latitude\":36.326731,\"longitude\":137.831840},{\"latitude\":36.327327,\"longitude\":137.831556},{\"latitude\":36.327716,\"longitude\":137.831245}]}"
         val serviceIntent = Intent(context, MockLocationJsonService::class.java)
         serviceIntent.putExtra(MockLocationJsonService.EXTRA_JSON, jsonString)
-        context.startService(serviceIntent)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(serviceIntent)
+        } else {
+            context.startService(serviceIntent)
+        }
     }
 
     private fun onReceiveStop() {
